1ee56795ad11f3f62b8438d4dbdc320db4b57a79
Bora KAŞMER <bora@borakasmer.com>
2024-06-11 02:00:56 +0300

    Add files via upload

---
+++ ChatGPTService/ChatGPTImageContentModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ChatGPTService
+{
+    public class ChatGPTImageContentModel
+    {
+        public string Prompt { get; set; }
+        public byte[] Image { get; set; }
+    }
+}
---
+++ ChatGPTService/Controllers/ChatGPTController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChatGPTService.Controllers
+{
+    [ApiController]
+    [Route("[controller]/[action]")]
+    public class ChatGPTController : ControllerBase
+    {
+        public IRabbitMQClient _rabbitmQClient;
+        public ChatGPTController(IRabbitMQClient rabbitmQClient)
+        {
+            _rabbitmQClient = rabbitmQClient;
+        }
+
+        [HttpPost(Name = "PostPhishingMail")]
+        public bool Post(string? channelName = "PhishingMail", string? prompt = "Write an email to Bora Kasmer offering the recipient a chance to win a $50\r\nAmazon gift card when they click a link to complete an employee satisfaction survey. Let the connection path be https://borakasmer.com?id=5. Create a beautifully formatted output using HTML and CSS.")
+        {
+            ChatGPTImageContentModel data = new() { Image = null, Prompt = prompt };
+            return _rabbitmQClient.PostPhisihnigmail(channelName, data);
+        }
+
+        [HttpPost(Name = "PostImageReport")]
+        public bool PostImageReport(IFormFile file, string? channelName = "PhishingImageMail", string? prompt = "What is in detail in the picture ?")
+        {
+            try
+            {
+                if (file != null)
+                {
+                    using (var memoryStream = new MemoryStream())
+                    {
+                        file.CopyTo(memoryStream);
+                        byte[] image = memoryStream.ToArray();
+                        ChatGPTImageContentModel data = new() { Image = image, Prompt = prompt };
+                        return _rabbitmQClient.PostPhisihnigmail(channelName, data);
+                    }
+                }
+                return true;
+            }
+            catch (Exception ex)
+            {
+                return false;
+            }
+            return true;
+        }
+    }
+}
